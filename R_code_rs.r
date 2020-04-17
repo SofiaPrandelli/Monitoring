8813f194834a6923d0a387ed6f92c9d0a28beb7d
SofiaPrandelli <62661924+SofiaPrandelli@users.noreply.github.com>
2020-04-17 17:57:06 +0200

    Create R_code_rs.r

---
+++ R_code_rs.r
@@ -0,0 +1,7 @@
+# R code for remote sensing data analysis
+
+# we install the packages: raster
+install.packages("raster")
+install.packages("RStoolbox")
+
+# oppure: install.packages(c("raster","RStoolbox"))
