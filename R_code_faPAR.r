95fe1430d42c14a9f9b9e40755a58e2bce22971e
SofiaPrandelli <62661924+SofiaPrandelli@users.noreply.github.com>
2020-05-13 18:04:19 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -3,19 +3,18 @@
 
 # install.packages("raster")
 
- 
+ setwd("/Users/sofiaprandelli/lab")
 
 library(raster)
 library(rasterVis)
 library(rasterdiv)
 
 plot(copNDVI9)
-
 copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
-levelplot(copNDVI)
 
-setwd("/Users/sofiaprandelli/lab") 
+levelplot(copNDVI)
 faPAR10 <- raster("faPAR10.tif")
+
 levelplot(faPAR10)
 |
 # WHAT HAPPEN IN THIS CASE? differences with the previous graph? there's a big rate of photosyntesis in the euator band
@@ -24,6 +23,7 @@ levelplot(faPAR10)
 # DIFFERENT CAPABILITY OF TAKING LIGHT 
 #carbon uptake of plants--> very high in tropical forests 
 
+#save the plot as pdf
 pdf("copNDVI.pdf")
 levelpot(copNDVI)
 dev.off()
@@ -35,5 +35,22 @@ dev.off()                     # to safe plots as PDF from R
 
 
 
-
 # estimate where is the relation between NDVI and faPAR 
+
+####################################################### day 2
+
+setwd("C:/lab/") 
+load("faPAR.RData")
+library(raster)
+library(rasterdiv)
+library (rasterVis)
+
+#the original faPAR from Copernicus is  2GB
+# let's see how much space is needed for an 8-bit set
+
+writeRaster( copNDVI, "copNDVI.tif")
+# 5.3 MB
+
+# to make the level plot of the faPAR
+levelplot(faPAR10) #faPAR = fraction of the solar radiation absorbed by live leaves 
+
