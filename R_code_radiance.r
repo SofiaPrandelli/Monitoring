ac6e7981a0cc54cdada8a0ec8ede65a8892652e6
SofiaPrandelli <62661924+SofiaPrandelli@users.noreply.github.com>
2020-05-13 18:01:40 +0200

    Create R_code_radiance.r

---
+++ R_code_radiance.r
@@ -0,0 +1,66 @@
+# R_code_radiance.r
+
+library(raster)
+library(rasterVis)
+library(rasterdiv)
+
+toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)
+
+# now we are going to put some values in the raster toy 
+values(toy) <- c(1.13,1.44,1.55,3.4)
+
+# now we put the datas into EACH PIXEL
+plot(toy)
+text(toy, digits=2)
+
+toy2bits <- stretch(toy,minv=0,maxv=3)
+storage.mode(toy2bits[]) = "integer"
+plot(toy2bits)
+text(toy2bits, digits=2)
+
+toy4bits <- stretch(toy,minv=0,maxv=15)
+storage.mode(toy4bits[]) = "integer"
+plot(toy4bits)
+text(toy4bits, digits=2)
+
+toy8bits <- stretch(toy,minv=0,maxv=255)
+storage.mode(toy8bits[]) = "integer"
+
+plot(toy8bits)
+text(toy8bits, digits=2)
+
+par(mfrow=c(1,4))
+
+plot(toy)
+text(toy, digits=2) 
+
+plot(toy2bits)
+text(toy2bits, digits=2)
+
+plot(toy4bits)
+text(toy4bits, digits=2)
+
+plot(toy8bits)
+text(toy8bits, digits=2)
+
+############################################ day2
+
+#now we import the FILE FAPAR10
+
+setwd("/users/sofiaprandelli/lab")
+load("faPAR.RData")
+library(raster)
+library(rasterdiv)
+library (rasterVis)
+
+# THE ORIGINAL faPAR from Copernicus is 2GB
+#lets see how much space is needed for an 8-bit set
+
+#let's write a rasterfile outside R
+
+
+writeRaster(copNDVI, "copNDVI.tif")
+# 5.3 MB 
+
+# EXERCISE: make the level plot of the faPAR
+levelplot(faPAR10) #faPAR = fraction of the solar radiation absorbed by live leaves 
