abad742c264b8bd7c8bc178ca160b8ade0d1ff78
SofiaPrandelli <62661924+SofiaPrandelli@users.noreply.github.com>
2020-06-03 17:26:02 +0200

    Update R_code_snow.r

--- R_code_snow.r
+++ R_code_snow.r
@@ -9,7 +9,7 @@ library(raster)
 
 snowmay <- raster("c_gls_SCE500_202005180000_CEURO_MODIS_V1.0.1.nc") #giving the name "snow may"
 # warning message: in general these images are covering all the world, but into copernicus we are downloading just a part of the image.
-#                 It says we are just using a part of the reference system, so the software cananot process the part of the image we didnt download
+#                  It says we are just using a part of the reference system, so the software cananot process the part of the image we didnt download
 
 # snow: associated with blue color, so we choose a blue palette 
 cl <- colorRampPalette(c('darkblue','blue','light blue'))(100)
@@ -63,8 +63,48 @@ plot(snow.multitemp, col=cl)
 
 source("prediction.r")    #source function: it causes R to accept its input from the named file/URL/connection or expressions directly
 
+plot(predicted.snow.2025.norm, col=cl)
 
 
 
+############################# Lesson 3/05 - day 2nd
+
+setwd("/Users/sofiaprandelli/lab/snow")
+
+# Exercise: import all together the snow cover images
+library(raster)
+
+rlist <- list.files(pattern="snow")   # as we did previously 
+rlist
+
+import <- lapply(rlist, raster)
+snow.multitemp <- stack(import)
+plot(snow.multitemp, col=cl)
+
+cl <- colorRampPalette(c('darkblue','blue','light blue'))(100) 
+plot(snow.multitemp, col=cl)
+
+# if you have saved the document on lab --> load("prediction.RData")
+# if not: download into snow folder the file prediction outputFile on IOL:
+prediction <- raster("predicted.2025.norm.tif")
+
+plot(prediction, col=cl)   # let's plot the prediction using the color we choose before (blue ramp palette) 
+
+# let's export the output: for example if we made the calculation and we want to send the output to a collegue
+
+writeRaster(prediction, "final.tif") #the fn creates a data, not just exporting a graph
+                                     # it's the opposite of raster fn: whiteR let us to export from R to the folder
+# final stack 
+final.stack <- stack(snow.multitemp, prediction)
+# 1) at the end of the process, we had the input layers as a stack (pila) wich for us is snow.multitemp
+# 2) then we made the map "prediction"
+# 3) now we made a final stack composed by the first stack and the prediction: we have 2000,2005,2010,2015,2020 + 2025 
+final.stack <- stack(snow.multitemp, prediction)
+plot(final.stack, col=cl)
+
+# let's  EXPORT THE R GRAPH AS PDF --> you'll find it in lab folder! ˆIMPORTANT!
+pdf("my_final_exciting_graph.pdf")
+plot(final.stack, col=cl)
+dev.off()
 
 
