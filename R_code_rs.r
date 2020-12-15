51d09d27274fb30f406e769fadfc97462c4c793c
SofiaPrandelli <62661924+SofiaPrandelli@users.noreply.github.com>
2020-12-15 14:47:05 +0100

    Delete R_code_rs.r

--- R_code_rs.r
+++
@@ -1,7 +0,0 @@
-# R code for remote sensing data analysis
-
-# we install the packages: raster
-install.packages("raster")
-install.packages("RStoolbox")
-
-# oppure: install.packages(c("raster","RStoolbox"))
